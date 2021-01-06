a3b5007287ada4c8eb626dac52654615aac4ec47
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-01-06 20:20:56 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import {ReactComponent as YouTubeIcon} from './youtube.svg';
+import {ReactComponent as LinkedInIcon} from './linkedin.svg';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/" target="_new"><YouTubeIcon /></a>
+        <a href="https://www.linkedin.com/" target="_new"><LinkedInIcon /></a>
+        <a href="https://www.instagram.com/" target="_new"><InstagramIcon /></a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
